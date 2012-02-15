b31eb619f4b1beae22e441894e19218575686347
pmonjallon <p.monjallon@gmail.com>
2012-02-15 15:25:44 +0100

    Lancement websocket debut client.

--- network.js
+++ network.js
@@ -2,26 +2,27 @@
  * Gère la partie réseau (connexions) du client.
  */
 
-  this.connected = false;
   
 var wsuri = "ws://localhost:9000";
 var webSocket;
 
-            if ("WebSocket" in window) {
-               webSocket = new WebSocket(wsuri);
-            }
-            else {
-               // Firefox currently prefixes the WebSocket object
-               webSocket = new MozWebSocket(wsuri);
-            }
+$(function() {
 
-            webSocket.onmessage = function(e) {
-               alert("Got echo: " + e.data);
-            }
-         
-  
-  this.sendNewRule = function(message) {
-		webSocket.send(JSON.stringify(message));
-  };
+	if ("WebSocket" in window) {
+		webSocket = new WebSocket(wsuri);
+	}
+	else {
+	// Firefox currently prefixes the WebSocket object
+		webSocket = new MozWebSocket(wsuri);
+	}
+
+	webSocket.onmessage = function(e) {
+		alert("Got echo: " + e.data);
+	}
+});			 
+
+function sendNewRule(message) {
+	webSocket.send(JSON.stringify(message));
+};
   
   
