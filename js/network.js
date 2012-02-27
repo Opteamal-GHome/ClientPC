354666daee18154addaf8770a9ac4fa6a4225905
Thomas FATTAL <thomas.fattal@insa-lyon.fr>
2012-02-27 20:50:00 +0100

    Suppression du +

--- js/network.js
+++ js/network.js
@@ -3,7 +3,7 @@
  */
 
   //"ws://localhost:8081"; //
-var wsuri =  + $('#urlServer').text().split(":")[0] + ":8081";
+var wsuri = $('#urlServer').text().split(":")[0] + ":8081";
 var webSocket;
 
 $(function() {
