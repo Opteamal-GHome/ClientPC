032ae814329ecfd01c307993f85e9ccfc453fb68
pmonjallon <p.monjallon@gmail.com>
2012-02-29 19:34:45 +0100

    Ajout modification priorite.

--- js/client_admin_regle_script.js
+++ js/client_admin_regle_script.js
@@ -6,7 +6,7 @@ $(function() {
 			var nomRegle = $(this).parent().text();
 			console.log("nom = " + nomRegle);
 			
-			var data =  {"msgType": "rule_removed", 
+			var data =  {"msgType":"rule_removed", 
 						 "rule":nomRegle
 						};
 		
@@ -14,7 +14,22 @@ $(function() {
 	});
 	
 	// On rend les règles classables
-	$( "#liste_regles" ).sortable();
+	$( "#liste_regles" ).sortable({
+		update: function(event, ui) {
+			
+			var arrayRules = new Array();
+			$('#liste_regles li.regle_desc').each(function(i) {
+				arrayRules.push($('div.top_regle', this).text().replace(/\t/g, '').replace(/\n/g, ''));
+			});
+			
+			var data =  {"msgType":"priorities", 
+						 "rules":arrayRules
+						};
+
+			console.log("to send:"+data.rules);
+			sendJson(data);
+		}
+	});
 	$( "#liste_regles" ).disableSelection();
 });
 		
